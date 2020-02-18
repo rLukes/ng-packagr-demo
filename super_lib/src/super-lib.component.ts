8b084d84dc5d7ddb8cabf079dd6bd1a7e3c58eb6
Ruben Lukes <lukesruben@gmail.com>
2020-02-18 14:01:12 +0100

    adding simple component to work with

---
+++ super_lib/src/super-lib.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-name",
+  template: "<p>Hello from the library</p>"
+})
+export class SuperLibComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
